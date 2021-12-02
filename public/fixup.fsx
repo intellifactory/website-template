999f3fb5661bf7f2c61265b1253f8bbc106ee208
Adam Granicz <adam.granicz@gmail.com>
2021-12-02 02:17:27 +0100

    HTML files need to revert &amp; to &.

--- public/fixup.fsx
+++ public/fixup.fsx
@@ -5,5 +5,5 @@ Directory.EnumerateFiles(Environment.CurrentDirectory, "*.html")
 |> Seq.toList
 |> List.iter (fun path ->
     let txt = File.ReadAllText(path)
-    File.WriteAllText(path, txt.Replace("%24%7B", "${").Replace("%7D", "}"))
+    File.WriteAllText(path, txt.Replace("%24%7B", "${").Replace("%7D", "}").Replace("&amp;", "&"))
 )
